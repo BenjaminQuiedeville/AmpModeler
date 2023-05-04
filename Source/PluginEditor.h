fcbffde819ae3038e22dacd474ba73827a63824d
Benjamin Quiedeville <84285984+BenjaminQuiedeville@users.noreply.github.com>
2023-05-04 18:39:34 +0200

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -29,18 +29,20 @@ private:
 
 
     juce::Slider bassEQSlider;
-    juce::Slider midEQSlider;
-    juce::Slider trebbleEQSlider;
     juce::Label bassEQSliderLabel {"BASS_EQ_SLIDER_LABEL", "Bass"};
-    juce::Label midEQSliderLabel {"MIDDLE_EQ_SLIDER_LABEL", "Mid"};
-    juce::Label trebbleEQSliderLabel {"TREBBLE_EQ_SLIDER_LABEL", "Trebble"};
     std::unique_ptr<SliderAttachment> bassEQSliderAttachment;
+
+    juce::Slider midEQSlider;
+    juce::Label midEQSliderLabel {"MIDDLE_EQ_SLIDER_LABEL", "Mid"};
     std::unique_ptr<SliderAttachment> midEQSliderAttachment;
+
+    juce::Slider trebbleEQSlider;
+    juce::Label trebbleEQSliderLabel {"TREBBLE_EQ_SLIDER_LABEL", "Trebble"};
     std::unique_ptr<SliderAttachment> trebbleEQSliderAttachment;
 
 
     juce::Slider gainSlider;
-    juce::Label gainSliderLabel;
+    juce::Label gainSliderLabel {"GAIN_SLIDER_LABEL", "Pre Gain"};
     std::unique_ptr<SliderAttachment> gainSliderAttachment;
 
 	juce::Slider volumeSlider;
