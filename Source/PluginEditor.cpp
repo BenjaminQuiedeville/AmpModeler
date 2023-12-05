92ba65eeb284f812a54351fcc9a11d58f2338982
Benjamin Quiédeville <84285984+BenjaminQuiedeville@users.noreply.github.com>
2023-12-05 13:54:24 +0100

    add Knob struct

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -6,18 +6,48 @@ AmpModelerAudioProcessorEditor::AmpModelerAudioProcessorEditor (AmpModelerAudioP
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
 
-    createKnob(gateSlider, gateSliderLabel, gateSliderAttachment, ParamsID[GATE_THRESH]);
+    gateKnob         = std::make_unique<Knob>("GATE_KNOB_LABEL", "Gate Thresh");
+    boostTopKnob     = std::make_unique<Knob>("BOOST_TOP_KNOB_LABEL" , "Boost Top");
+    boostTightKnob   = std::make_unique<Knob>("BOOST_TIGHT_KNOB_LABEL", "Boost Tight");
+    gainKnob         = std::make_unique<Knob>("GAIN_KNOB_LABEL", "Pre Gain");
+    inputFilterKnob  = std::make_unique<Knob>("INPUT_KNOB_FILTER_LABEL", "Input Filter");
+    bassEQKnob       = std::make_unique<Knob>("BASS_EQ_KNOB_LABEL", "Bass");
+    midEQKnob        = std::make_unique<Knob>("MIDDLE_EQ_KNOB_LABEL", "Mid");
+    trebbleEQKnob    = std::make_unique<Knob>("TREBBLE_EQ_KNOB_LABEL", "Trebble");
+    preampVolumeKnob = std::make_unique<Knob>("PREMP_VOLUME_KNOB_LABEL", "Post Gain");
+    volumeKnob       = std::make_unique<Knob>("MASTER_VOLUME_KNOB_LABEL", "Output Level");
 
-    createKnob(gainSlider, gainSliderLabel, gainSliderAttachment, ParamsID[PREAMP_GAIN]);
 
-    createKnob(bassEQSlider, bassEQSliderLabel, bassEQSliderAttachment, ParamsID[TONESTACK_BASS]);
-    createKnob(midEQSlider, midEQSliderLabel, midEQSliderAttachment, ParamsID[TONESTACK_MIDDLE]);
-    createKnob(trebbleEQSlider, trebbleEQSliderLabel, trebbleEQSliderAttachment, ParamsID[TONESTACK_TREBBLE]);
+    // createKnob(gateSlider, gateSliderLabel, gateSliderAttachment, ParamsID[GATE_THRESH]);
 
-    createKnob(preampVolumeSlider, preampVolumeSliderLabel, preampVolumeSliderAttachment, ParamsID[PREAMP_VOLUME]);
+    // createKnob(gainSlider, gainSliderLabel, gainSliderAttachment, ParamsID[PREAMP_GAIN]);
 
+    // createKnob(inputFilterSlider, inputFilterSliderLabel, inputFilterSliderAttachment, ParamsID[INPUT_FILTER]);
 
-    createKnob(volumeSlider, volumeSliderLabel, volumeSliderAttachment, ParamsID[MASTER_VOLUME]);
+    // createKnob(bassEQSlider, bassEQSliderLabel, bassEQSliderAttachment, ParamsID[TONESTACK_BASS]);
+    // createKnob(midEQSlider, midEQSliderLabel, midEQSliderAttachment, ParamsID[TONESTACK_MIDDLE]);
+    // createKnob(trebbleEQSlider, trebbleEQSliderLabel, trebbleEQSliderAttachment, ParamsID[TONESTACK_TREBBLE]);
+
+    // createKnob(preampVolumeSlider, preampVolumeSliderLabel, preampVolumeSliderAttachment, ParamsID[PREAMP_VOLUME]);
+
+    // createKnob(volumeSlider, volumeSliderLabel, volumeSliderAttachment, ParamsID[MASTER_VOLUME]);
+
+    createKnob(gateKnob.get(), ParamsID[GATE_THRESH]);
+    createKnob(boostTopKnob.get(), ParamsID[BITE]);
+    createKnob(boostTightKnob.get(), ParamsID[TIGHT]);
+
+    createKnob(gainKnob.get(), ParamsID[PREAMP_GAIN]);
+
+    createKnob(inputFilterKnob.get(), ParamsID[INPUT_FILTER]);
+
+    createKnob(bassEQKnob.get(), ParamsID[TONESTACK_BASS]);
+    createKnob(midEQKnob.get(), ParamsID[TONESTACK_MIDDLE]);
+    createKnob(trebbleEQKnob.get(), ParamsID[TONESTACK_TREBBLE]);
+
+    createKnob(preampVolumeKnob.get(), ParamsID[PREAMP_VOLUME]);
+
+    createKnob(volumeKnob.get(), ParamsID[MASTER_VOLUME]);
+
 
     irLoadButton.onClick = [&]() { audioProcessor.irLoader->loadIR(); };
     addAndMakeVisible(irLoadButton);
@@ -50,45 +80,81 @@ void AmpModelerAudioProcessorEditor::resized() {
     auto computeXcoord = [&](int col) { return horizontalMargin + WIDTH/nCols * col; };
     auto computeYcoord = [&](int row) { return verticalMargin + HEIGHT/nRows * row; };
 
-    gateSlider.setBounds(computeXcoord(0), computeYcoord(0), knobSize, knobSize);
-    gateSliderLabel.setBounds(gateSlider.getX(), gateSlider.getY() - 20, gateSlider.getWidth(), 20);
+    gateKnob->slider.setBounds(computeXcoord(0), computeYcoord(0), knobSize, knobSize);
+    gateKnob->label.setBounds(gateKnob->slider.getX(), 
+                              gateKnob->slider.getY() - 20, 
+                              gateKnob->slider.getWidth(), 
+                              20);
 
-    gainSlider.setBounds(computeXcoord(1), computeYcoord(1), knobSize, knobSize);
-    gainSliderLabel.setBounds(gainSlider.getX(), gainSlider.getY() - 20, gainSlider.getWidth(), 20);
+    boostTopKnob->slider.setBounds(computeXcoord(0), computeYcoord(1), knobSize, knobSize);
+    boostTopKnob->label.setBounds(boostTopKnob->slider.getX(),
+                                  boostTopKnob->slider.getY() - 20,
+                                  boostTopKnob->slider.getWidth(), 
+                                  20);
+
+    boostTightKnob->slider.setBounds(computeXcoord(0), computeYcoord(2), knobSize, knobSize);
+    boostTightKnob->label.setBounds(boostTightKnob->slider.getX(),
+                                    boostTightKnob->slider.getY() - 20,
+                                    boostTightKnob->slider.getWidth(),
+                                    20);
+
+    gainKnob->slider.setBounds(computeXcoord(1), computeYcoord(1), knobSize, knobSize);
+    gainKnob->label.setBounds(gainKnob->slider.getX(), 
+                              gainKnob->slider.getY() - 20, 
+                              gainKnob->slider.getWidth(), 
+                              20);
     
-    bassEQSlider.setBounds(computeXcoord(2), computeYcoord(1), knobSize, knobSize);
-    bassEQSliderLabel.setBounds(bassEQSlider.getX(), bassEQSlider.getY() - 20, bassEQSlider.getWidth(), 20);
+    inputFilterKnob->slider.setBounds(computeXcoord(1), computeYcoord(2), knobSize, knobSize);
+    inputFilterKnob->label.setBounds(inputFilterKnob->slider.getX(), 
+                                     inputFilterKnob->slider.getY() - 20, 
+                                     inputFilterKnob->slider.getWidth(), 
+                                     20);
 
-    midEQSlider.setBounds(computeXcoord(3), computeYcoord(1), knobSize, knobSize);
-    midEQSliderLabel.setBounds(midEQSlider.getX(), midEQSlider.getY() - 20, midEQSlider.getWidth(), 20);
+    bassEQKnob->slider.setBounds(computeXcoord(2), computeYcoord(1), knobSize, knobSize);
+    bassEQKnob->label.setBounds(bassEQKnob->slider.getX(), 
+                                bassEQKnob->slider.getY() - 20, 
+                                bassEQKnob->slider.getWidth(), 
+                                20);
+
+    midEQKnob->slider.setBounds(computeXcoord(3), computeYcoord(1), knobSize, knobSize);
+    midEQKnob->label.setBounds(midEQKnob->slider.getX(), 
+                               midEQKnob->slider.getY() - 20, 
+                               midEQKnob->slider.getWidth(), 
+                               20);
     
-    trebbleEQSlider.setBounds(computeXcoord(4), computeYcoord(1), knobSize, knobSize);
-    trebbleEQSliderLabel.setBounds(trebbleEQSlider.getX(), trebbleEQSlider.getY() - 20, trebbleEQSlider.getWidth(), 20);
+    trebbleEQKnob->slider.setBounds(computeXcoord(4), computeYcoord(1), knobSize, knobSize);
+    trebbleEQKnob->label.setBounds(trebbleEQKnob->slider.getX(), 
+                                   trebbleEQKnob->slider.getY() - 20, 
+                                   trebbleEQKnob->slider.getWidth(), 
+                                   20);
 
-    preampVolumeSlider.setBounds(computeXcoord(5), computeYcoord(1), knobSize, knobSize);
-    preampVolumeSliderLabel.setBounds(preampVolumeSlider.getX(), preampVolumeSlider.getY() - 20, preampVolumeSlider.getWidth(), 20);
+    preampVolumeKnob->slider.setBounds(computeXcoord(5), computeYcoord(1), knobSize, knobSize);
+    preampVolumeKnob->label.setBounds(preampVolumeKnob->slider.getX(), 
+                                      preampVolumeKnob->slider.getY() - 20, 
+                                      preampVolumeKnob->slider.getWidth(), 
+                                      20);
 
-	volumeSlider.setBounds(computeXcoord(5), computeYcoord(2), knobSize, knobSize);
-    volumeSliderLabel.setBounds(volumeSlider.getX(), volumeSlider.getY() - 20, volumeSlider.getWidth(), 20);
+	volumeKnob->slider.setBounds(computeXcoord(5), computeYcoord(2), knobSize, knobSize);
+    volumeKnob->label.setBounds(volumeKnob->slider.getX(), 
+                                volumeKnob->slider.getY() - 20, 
+                                volumeKnob->slider.getWidth(), 
+                                20);
 
     irLoadButton.setBounds(computeXcoord(5), computeYcoord(0), 100, 50);
 
 }
 
 
-void AmpModelerAudioProcessorEditor::createKnob(juce::Slider &slider, 
-                                                juce::Label &label, 
-                                                std::unique_ptr<SliderAttachment> &sliderAttachmnt, 
-                                                const juce::String &paramID)
-{
-    slider.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
-    slider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 50, 25);
-    addAndMakeVisible(slider);
+void AmpModelerAudioProcessorEditor::createKnob(Knob *knob, const juce::String& paramID) {
 
-    label.setColour(juce::Label::ColourIds::textColourId, juce::Colours::white);
-    label.setJustificationType(juce::Justification::centred);
-    label.setFont(15.0f);
-    addAndMakeVisible(label);
+    knob->slider.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+    knob->slider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 50, 25);
+    addAndMakeVisible(knob->slider);
 
-    sliderAttachmnt = std::make_unique<SliderAttachment>(*(audioProcessor.apvts), paramID, slider);
+    knob->label.setColour(juce::Label::ColourIds::textColourId, juce::Colours::white);
+    knob->label.setJustificationType(juce::Justification::centred);
+    knob->label.setFont(15.0f);
+    addAndMakeVisible(knob->label);
+
+    knob->sliderAttachment = std::make_unique<SliderAttachment>(*(audioProcessor.apvts), paramID, knob->slider);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -4,6 +4,17 @@
 #include "PluginProcessor.h"
 
 
+struct Knob {
+
+    Knob(juce::String labelID, juce::String name) : label{labelID, name} {
+    }
+
+    juce::Slider slider;
+    juce::Label label;
+    std::unique_ptr<SliderAttachment> sliderAttachment;
+
+};
+
 class AmpModelerAudioProcessorEditor  : public juce::AudioProcessorEditor
 {
 public:
@@ -16,34 +27,49 @@ public:
 
 private:
 
-    juce::Slider gateSlider;
-    juce::Label gateSliderLabel {"GATE_SLIDER_LABEL", "Gate Thresh"};
-    std::unique_ptr<SliderAttachment> gateSliderAttachment; 
+    std::unique_ptr<Knob> gateKnob;
+    std::unique_ptr<Knob> boostTopKnob;
+    std::unique_ptr<Knob> boostTightKnob;
+    std::unique_ptr<Knob> gainKnob;
+    std::unique_ptr<Knob> inputFilterKnob;
+    std::unique_ptr<Knob> bassEQKnob;
+    std::unique_ptr<Knob> midEQKnob;
+    std::unique_ptr<Knob> trebbleEQKnob;
+    std::unique_ptr<Knob> preampVolumeKnob;
+    std::unique_ptr<Knob> volumeKnob;
 
-    juce::Slider gainSlider;
-    juce::Label gainSliderLabel {"GAIN_SLIDER_LABEL", "Pre Gain"};
-    std::unique_ptr<SliderAttachment> gainSliderAttachment;
+    // juce::Slider gateSlider;
+    // juce::Label gateSliderLabel {"GATE_SLIDER_LABEL", "Gate Thresh"};
+    // std::unique_ptr<SliderAttachment> gateSliderAttachment; 
 
-    juce::Slider bassEQSlider;
-    juce::Label bassEQSliderLabel {"BASS_EQ_SLIDER_LABEL", "Bass"};
-    std::unique_ptr<SliderAttachment> bassEQSliderAttachment;
+    // juce::Slider gainSlider;
+    // juce::Label gainSliderLabel {"GAIN_SLIDER_LABEL", "Pre Gain"};
+    // std::unique_ptr<SliderAttachment> gainSliderAttachment;
 
-    juce::Slider midEQSlider;
-    juce::Label midEQSliderLabel {"MIDDLE_EQ_SLIDER_LABEL", "Mid"};
-    std::unique_ptr<SliderAttachment> midEQSliderAttachment;
+    // juce::Slider inputFilterSlider;
+    // juce::Label inputFilterSliderLabel {"INPUT_FILTER_LABEL", "Input Filter"};
+    // std::unique_ptr<SliderAttachment> inputFilterSliderAttachment;
 
-    juce::Slider trebbleEQSlider;
-    juce::Label trebbleEQSliderLabel {"TREBBLE_EQ_SLIDER_LABEL", "Trebble"};
-    std::unique_ptr<SliderAttachment> trebbleEQSliderAttachment;
+    // juce::Slider bassEQSlider;
+    // juce::Label bassEQSliderLabel {"BASS_EQ_SLIDER_LABEL", "Bass"};
+    // std::unique_ptr<SliderAttachment> bassEQSliderAttachment;
 
-    juce::Slider preampVolumeSlider;
-    juce::Label preampVolumeSliderLabel {"PREMP_VOLUME_SLIDER_LABEL", "Post Gain"};
-    std::unique_ptr<SliderAttachment> preampVolumeSliderAttachment;
+    // juce::Slider midEQSlider;
+    // juce::Label midEQSliderLabel {"MIDDLE_EQ_SLIDER_LABEL", "Mid"};
+    // std::unique_ptr<SliderAttachment> midEQSliderAttachment;
+
+    // juce::Slider trebbleEQSlider;
+    // juce::Label trebbleEQSliderLabel {"TREBBLE_EQ_SLIDER_LABEL", "Trebble"};
+    // std::unique_ptr<SliderAttachment> trebbleEQSliderAttachment;
+
+    // juce::Slider preampVolumeSlider;
+    // juce::Label preampVolumeSliderLabel {"PREMP_VOLUME_SLIDER_LABEL", "Post Gain"};
+    // std::unique_ptr<SliderAttachment> preampVolumeSliderAttachment;
 
 
-	juce::Slider volumeSlider;
-    juce::Label volumeSliderLabel {"MASTER_VOLUME_SLIDER_LABEL", "Output Level"};
-    std::unique_ptr<SliderAttachment> volumeSliderAttachment;
+	// juce::Slider volumeSlider;
+    // juce::Label volumeSliderLabel {"MASTER_VOLUME_SLIDER_LABEL", "Output Level"};
+    // std::unique_ptr<SliderAttachment> volumeSliderAttachment;
 
 
     juce::TextButton irLoadButton {"Load IR"};
@@ -51,10 +77,12 @@ private:
 
     AmpModelerAudioProcessor& audioProcessor;
 
-    void createKnob(juce::Slider &slider, 
-					juce::Label &label, 
-					std::unique_ptr<SliderAttachment> &sliderAttchmnt, 
-					const juce::String& paramID);
+    // void createKnob(juce::Slider &slider, 
+	// 				juce::Label &label, 
+	// 				std::unique_ptr<SliderAttachment> &sliderAttchmnt, 
+	// 				const juce::String& paramID);
+
+    void createKnob(Knob *knob, const juce::String& paramID);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AmpModelerAudioProcessorEditor)
 };
