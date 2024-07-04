ec10836a605a40a424fd9ddd4297ed9ac3aeab75
Benjamin Quiedeville <quiedevilleb@gmail.com>
2024-07-04 16:27:51 +0200

    tabs to space conversion

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -118,45 +118,45 @@ struct Processor  : public juce::AudioProcessor,
                                    , public juce::AudioProcessorARAExtension
                                    #endif
 {
-	//==============================================================================
-	Processor();
-	~Processor() override;
+    //==============================================================================
+    Processor();
+    ~Processor() override;
 
-	//==============================================================================
-	void prepareToPlay (double sampleRate, int samplesPerBlock) override;
-	void releaseResources() override;
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
 
-	#ifndef JucePlugin_PreferredChannelConfigurations
-	bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
-	#endif
+    #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+    #endif
 
-	void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
 
-	//==============================================================================
-	juce::AudioProcessorEditor* createEditor() override;
-	bool hasEditor() const override;
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
 
-	//==============================================================================
-	const juce::String getName() const override;
+    //==============================================================================
+    const juce::String getName() const override;
 
-	bool acceptsMidi() const override;
-	bool producesMidi() const override;
-	bool isMidiEffect() const override;
-	double getTailLengthSeconds() const override;
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
 
-	//==============================================================================
-	int getNumPrograms() override;
-	int getCurrentProgram() override;
-	void setCurrentProgram (int index) override;
-	const juce::String getProgramName (int index) override;
-	void changeProgramName (int index, const juce::String& newName) override;
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
 
-	//==============================================================================
-	void getStateInformation (juce::MemoryBlock& destData) override;
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
     juce::ValueTree valueTree;
-	juce::AudioProcessorValueTreeState apvts;
+    juce::AudioProcessorValueTreeState apvts;
 
     Biquad inputNoiseFilter {BIQUAD_LOWPASS};
 
@@ -171,7 +171,7 @@ struct Processor  : public juce::AudioProcessor,
     FirstOrderShelfFilter resonanceFilter {lowshelf};
     FirstOrderShelfFilter presenceFilter {highshelf};
 
-	SmoothParamLinear masterVolume;
+    SmoothParamLinear masterVolume;
 
     double samplerate = 44100.0;
 
@@ -182,7 +182,7 @@ struct Processor  : public juce::AudioProcessor,
     void initParameters();
     void parameterChanged(const juce::String &parameterID, float newValue) override;
 
-	juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
 
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Processor)
