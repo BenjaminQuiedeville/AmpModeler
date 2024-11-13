40ec54180580fc23fefef1358688bcb707b09a59
Benjamin Quiédeville <84285984+BenjaminQuiedeville@users.noreply.github.com>
2024-11-13 01:11:01 +0100

    Update tube_sim.jl

--- experiments/tube_sim.jl
+++ experiments/tube_sim.jl
@@ -1,17 +1,18 @@
 using GLMakie
-using Statistics
-using Chain
 using WAV
 using ProgressMeter
+using FFTW
+using Statistics
 
 include("clipping_curves.jl")
 
 function main() :: Figure
     samplerate :: Float64 = 48000.0
 
-    signal = 0.2 .* sin.(0:0.01:4*pi)
+    signal = 1.0 .* sin.(LinRange(0.0, 15*pi, 8192))
+    # signal = zeros(Float64, 1000)
 
-    # signal = 2 .* sinpi.(2 * 0:1/samplerate:0.7 * 200)
+    # signal = 2 .* sinpi.(2 * 0:1/samplerate:pi * 100)
     # signal .*= LinRange(0, 1, length(signal))
 
     # signal, samplerate, _, _ = wavread("data/Metallica.wav")
@@ -22,7 +23,8 @@ function main() :: Figure
 
     figure = Figure()
     ax = Axis(figure[1, 1])
-    lines!(signal, label = "input signal")
+    # lines!(signal * 100, label = "input signal")
+
     threshold = 1.0
     ratio = 4
     knee_width = 0.01
@@ -38,22 +40,27 @@ function main() :: Figure
         signal[index] = sample
     end
 
-    lines!(signal, label = "grid_conduction")
+    # lines!(signal, label = "grid_conduction")
 
     gain = 10^(35/20)
-
+    bias = 2.0
     for index in 1:length(signal)
         sample = -signal[index]
-        sample = asym_tanh(sample, -2.0, 0.0)
+        sample = asym_tanh(sample + bias, -2.0, 0.0)
         sample *= gain
 
         signal[index] = sample
     end 
 
-    lines!(-signal, label = "tube_output", color=:red)
+    signal .-= mean(signal)
+
+    lines!(signal, label = "tube_output", color=:red)
 
     axislegend(; position = :lb)
 
+    freq_axis = Axis(figure[2, 1], xscale = log10)
+    lines!(freq_axis, 20.0 * log10.(abs.(rfft(signal[1:8192]))))
+    ylims!(freq_axis, [0.0, 100.0])
     return figure
 end 
 figure = main()
